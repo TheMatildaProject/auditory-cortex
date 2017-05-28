56a46204ba497f2ae2e5ec6e83102cafc1dae495
Eduardo Leoni <eduardoleoni@otlook.com>
2017-05-28 18:09:11 +1200

    Make sure string is converted to bytes before decoding b64

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def api():
-    if not request.json or not 'audio' in request.json:
+    if not request.json:
+        return jsonify({'error': 'Json header expected'}), 401
+
+    if not 'audio' in request.json:
         return jsonify({'error': 'Missing audio'}), 400
 
     audio = request.json['audio']
 
-    stt = SpeechToText(base64.b64decode(audio))
+    stt = SpeechToText(base64.b64decode(audio.encode())) # string to byte before decoding b64
     text = stt.getFromGoogle()
     return jsonify({'text': text});
 
